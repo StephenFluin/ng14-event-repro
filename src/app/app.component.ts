7ede1953c585f3b97b774e7769f9cd9dd7b6ae89
Stephen Fluin <stephen.fluin@gmail.com>
2022-11-08 13:19:51 -0600

    add pages and expose event_2 router event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'event2-repro';
+  constructor(router: Router) {
+    router.events.pipe(filter(e => e instanceof NavigationStart)).subscribe(n => {})
+  }
 }
+``
